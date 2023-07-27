82287f63ca13c9b5377730055877f4ca4182ffc4
Bernardo Toresan <btoresan@MacBook-Air-de-Wilson-2.local>
2023-07-27 18:50:57 -0300

    valfa-test

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <raylib.h>
+
+int main()
+{
+    InitWindow(800, 600, "My Raylib Program");
+    
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, Raylib!", 10, 10, 20, BLACK);
+        EndDrawing();
+    }
+    
+    CloseWindow();
+    return 0;
+}
