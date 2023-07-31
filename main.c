9f7eb835343fe8409903a90488f10246c74716cc
Bernardo Toresan <btoresan@MacBook-Air-de-Wilson-2.local>
2023-07-31 17:22:13 -0300

    projeto arrumado v1.0

--- main.c
+++ main.c
@@ -1,27 +1,38 @@
-
-
-
-
-
-
-
-
-
-
 #include <raylib.h>
+#include "functions/menu.h" //puxa as funções dos arquivos menu
+
+int run(){
+
+    InitWindow(800, 600, "Labirento Del Saber"); //Inicializa a Tela
+
+    //variaveis para a mensagem de erro
+    int Xmeio = GetScreenWidth() / 2 - MeasureText("SORRY THERE WAS AN ERROR", 40) / 2;
+    int Ymeio = GetScreenHeight() / 2 - 40 / 2;
+    
+    SetTargetFPS(60); //define o jogo rodando em 60 FPS
+
+    while (!WindowShouldClose())
+    {
+        if (menu()){ //Se o menu devolver um erro carrega uma tela de erro
+            BeginDrawing();
+            ClearBackground(RED);
+
+            DrawText("SORRY THERE WAS AN ERROR", Xmeio, Ymeio, 40, WHITE);
+            
+            EndDrawing();
+        }
+
+    }
+    
+    CloseWindow(); //encerra o programa
+
+    return 0;
+}
+
 
 int main()
 {
-    InitWindow(800, 600, "My Raylib Program");
+    run();
     
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Wel come be", 10, 10, 20, BLACK);
-        EndDrawing();
-    }
-    
-    CloseWindow();
     return 0;
 }
