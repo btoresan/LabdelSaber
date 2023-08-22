52a020649cc297094f1a3c43ae4f18ea75b0df1b
leal0607 <gleal0607@gmail.com>
2023-08-22 18:01:18 -0300

    perguntas alpha

--- functions/jogo.c
+++ functions/jogo.c
@@ -2,17 +2,70 @@
 #include <raylib.h>
 
 
-int perguntas (){
+//Estrutura perguntas
+
+typedef struct
+{
+    char enunciado[200];
+    int nalternativas;
+    int altcorreta;
+    char alternativas [4][200];
+}PERGUNTA;
+
+
+int perguntas (PERGUNTA *vetperguntas){
     
-    //Inicializacao da saida
-    InitWindow(800, 600, "Labirinto Del Saber ALPHA");
+    int i,n,k;
     
-    while (!WindowShouldClose() && !IsKeyPressed(KEY_Q)){
-        BeginDrawing();
+    FILE *arqp;
+    
+    //Pega um valor aleatório que será a posição da pergunta escolhida
+    int nump = GetRandomValue(0,99);
+    
+    arqp = fopen("static/perguntas.txt","r");
+    
+    if (arqp==NULL)
+        printf("Falha na abertura do arquivo!");
+    
+    else
+    {
+        for(i=0;i<100;i++)
+        {
+            // Lê a estrutura de 100 perguntas (minimo)
+            fgets(vetperguntas[i].enunciado,sizeof(vetperguntas[i].enunciado),arqp);
+            fscanf(arqp,"%d",&vetperguntas[i].nalternativas);
+            fscanf(arqp,"%d",&vetperguntas[i].altcorreta);
+            
+            getc(arqp);
+            
+            for(n=0;n<4;n++)
+            {
+                fgets(vetperguntas[i].alternativas[n],sizeof(vetperguntas[i].alternativas[n]),arqp); 
+            }    
+        }
         
-        DrawText("INSIRA AS PERGUNTAS AQUI, APERTE Q PARA VOLTAR", 0, 300, 20, BLUE);
-        
-        EndDrawing();
+        //Inicializacao da saida
+        InitWindow(800, 600, "Labirinto Del Saber ALPHA");
+    
+        while (!WindowShouldClose() && !IsKeyPressed(KEY_Q))
+        {
+           
+            
+            BeginDrawing();
+            
+            // Desenha o enunciado
+            DrawText(vetperguntas[nump].enunciado, 0, 100, 20, BLUE);
+            
+            // Loop que desenha cada alternativa
+            for(k=0;k<4;k++)
+            {
+                DrawText(vetperguntas[nump].alternativas[k], 0, 200+k*80, 18, WHITE);        
+            }
+            
+            
+                
+            EndDrawing();
+        }
     }
     
     //Cleanup
@@ -31,6 +84,9 @@ int labirinto (){
     //Declaracao que salva o valor retornado de loadgame
     int saida;
     
+    //Declaracao do vetor de perguntas
+    PERGUNTA vetperguntas[100];
+    
     //Roda o jogo e salva a saida 
     saida = loadgame("static/mapas/mapa1.txt", 1, 1, 1, 30);
     
@@ -38,7 +94,7 @@ int labirinto (){
     switch (saida)
     {
         case 99:
-            perguntas();
+            perguntas(vetperguntas);
             break;
     }
     
