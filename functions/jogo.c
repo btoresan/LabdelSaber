e7558f83e06abb9f29b8d94e1bd5286b777364aa
leal0607 <gleal0607@gmail.com>
2023-08-22 19:09:22 -0300

    perguntas alpha

--- functions/jogo.c
+++ functions/jogo.c
@@ -15,6 +15,19 @@ typedef struct
 
 int perguntas (PERGUNTA *vetperguntas){
     
+    //Declaração de textura
+    Texture2D fundo_perguntas;
+    
+    
+    //valor que a função retornará
+    int saida = -1;
+    
+    
+    //Posição inicial da escolha
+    int pos_escolha=200;
+    
+    int pos_alt = 0;
+    
     int i,n,k;
     
     FILE *arqp;
@@ -45,13 +58,18 @@ int perguntas (PERGUNTA *vetperguntas){
         }
         
         //Inicializacao da saida
-        InitWindow(800, 600, "Labirinto Del Saber ALPHA");
+        InitWindow(1000, 500, "PERGUNTAS");
+        
+        fundo_perguntas = LoadTexture("static/fundo_perguntas.png");
     
-        while (!WindowShouldClose() && !IsKeyPressed(KEY_Q))
+        while (!WindowShouldClose() && saida==-1)
         {
            
             
             BeginDrawing();
+            
+            
+            DrawTexture(fundo_perguntas,0,0,WHITE);
             
             // Desenha o enunciado
             DrawText(vetperguntas[nump].enunciado, 0, 100, 20, BLUE);
@@ -59,11 +77,61 @@ int perguntas (PERGUNTA *vetperguntas){
             // Loop que desenha cada alternativa
             for(k=0;k<4;k++)
             {
-                DrawText(vetperguntas[nump].alternativas[k], 0, 200+k*80, 18, WHITE);        
+                DrawText(vetperguntas[nump].alternativas[k], 0, 200+k*70, 18, WHITE);        
             }
             
+            //Posição inicial da escolha (começa na primeira alternativa)
+            DrawText(vetperguntas[nump].alternativas[pos_alt], 0, pos_escolha, 18, RED);
             
+            if(IsKeyPressed(KEY_DOWN))
+            {
+                //Se a escolha estiver na ultima alternativa, volta para a primeira
+                if (pos_escolha == 410)
+                {
+                    pos_escolha = 200;
+                    pos_alt = 0;
+                }
                 
+                //Senão, desce para a proxima alternativa
+                else
+                {
+                    pos_escolha = pos_escolha + 70;  
+                    pos_alt = pos_alt +1;
+                }                    
+            }
+            
+            else if (IsKeyPressed(KEY_UP))
+            {
+                //Se a escolha estiver na primeira alternativa, vai para a ultima
+                if (pos_escolha == 200)
+                {
+                    pos_escolha = 410;
+                    pos_alt = 3;
+                }
+                
+                //Senão, sobe para a proxima alternativa
+                else
+                {
+                    pos_escolha = pos_escolha - 70;      
+                    pos_alt = pos_alt -1;
+                }                    
+            }
+            
+            // Se a escolha for correta, devolve 1
+            if(pos_alt+1==vetperguntas[nump].altcorreta)
+            {
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    saida = 1;
+                }
+            }
+            
+            //Senão devolve 0
+            else if (IsKeyPressed(KEY_ENTER))
+            {
+                saida = 0;
+            }
+
             EndDrawing();
         }
     }
@@ -74,7 +142,7 @@ int perguntas (PERGUNTA *vetperguntas){
     //Volta para o Labirinto
     labirinto();
     
-    return 0;
+    return saida;
 }
 
 
